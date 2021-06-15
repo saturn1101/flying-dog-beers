e96cf49ee51f5398947c3378ef2642f0be37d8a8
saturn1101 <phuongttn50.hrc@gmail.com>
2021-06-15 10:58:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from datetime import datetime as dt
 st.set_page_config(layout="wide")
 
 st.title('DataCracy ATOM Tiến Độ Lớp Học')
-with open('./env_variable.json','r') as j:
+with open('./flying-dog-beers/env_variable.json','r') as j:
     json_data = json.load(j)
 
 #SLACK_BEARER_TOKEN = os.environ.get('SLACK_BEARER_TOKEN') ## Get in setting of Streamlit Share
